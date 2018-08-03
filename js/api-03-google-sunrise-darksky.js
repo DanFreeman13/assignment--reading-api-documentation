8b4c0eefd89f3042bf568b2ac179d26eaaba8990
DanFreeman13 <dan_freeman@live.com.mx>
2018-08-02 22:27:35 -0500

    finished part 2 exercises 3 and 4

--- js/api-03-google-sunrise-darksky.js
+++ js/api-03-google-sunrise-darksky.js
@@ -12,6 +12,14 @@
 
 */
 
+var montrealCoord = [];
+const answerElement_apimashup_1 = document.getElementById('apimashup-1')
+const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+
+request
+.get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+.then(function(response) {
 
 //========================================================================
 //  (1) What are latitude longitude coordinates of Montreal?
@@ -20,15 +28,27 @@
 //     = NOtE: You don't need an API key for this api
 //
 
-const answerElement_apimashup_1 = document.getElementById('apimashup-1')
+  var location = response.body;
+  var props = location.results[0];
+  var geometrySpecs = props.geometry;
+  montrealCoord.push(geometrySpecs.location.lat + "");
+  montrealCoord.push(geometrySpecs.location.lng + "");
 
-
-
+  answerElement_apimashup_1.textContent = "Latitud = " + montrealCoord[0] + " & Longitud = " + montrealCoord[1];
 
 //========================================================================
 //  (2) What time does the sun set in Montreal (based on the Google geocode coordinates)?
 
-const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+  var API_URL_COORDS = "https://api.sunrise-sunset.org/json?lat=" + montrealCoord[0] + "&lng=" + montrealCoord[1];
+
+  console.log()
+
+  request
+  .get(API_URL_COORDS)
+  .then(function(newInfo) {
+    answerElement_apimashup_2.textContent = newInfo.body.results.sunset;
+  });
+
 
 
 
@@ -37,8 +57,20 @@ const answerElement_apimashup_2 = document.getElementById('apimashup-2')
 
 //========================================================================
 //  (3) What is the weekly weather forecast in Montreal? (look for summary property in 'daily')
+var API_KEY_DRKSKY = "a44050f5ec0c0ac41c4c8aa5dd833d20"
+var API_DRKSKY_URL_COORDS = "https://api.darksky.net/forecast/" + API_KEY_DRKSKY + "/" + montrealCoord[0] + "," + montrealCoord[1];
 
-const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+  request
+  .get(API_DRKSKY_URL_COORDS)
+  .then(function(weatherInfo) {
+    answerElement_apimashup_3.textContent = weatherInfo.body.daily.summary;
+  });
+
+});
+
+
+
+
 
 
 
--- js/api-04-tv-maze.js
+++ js/api-04-tv-maze.js
@@ -8,4 +8,39 @@ https://www.tvmaze.com/ap
 
 3. How many shows has actor Andrew Grimes (of the Walking Dead) appeared in?
 */
-*/
+var request = superagent;
+
+// 1. Average raiting of "Better Call Saul"
+
+const answerElement_tvmaze_1 = document.getElementById('tvmaze-1')
+
+request
+.get('http://api.tvmaze.com/search/shows?q=Better-call-saul')
+.then(function(response) {
+  var averageRating = response.body[0].show.rating.average;
+
+  answerElement_tvmaze_1.textContent = averageRating;
+});
+
+// 2. Premiere Date for Friends
+
+const answerElement_tvmaze_2 = document.getElementById('tvmaze-2')
+
+request
+.get('http://api.tvmaze.com/shows/431/seasons')
+.then(function(response) {
+  var premieredDate = response.body[8].premiereDate
+  answerElement_tvmaze_2.textContent = premieredDate;
+})
+
+// 3 shows has appeared actor Andrew Lincoln
+
+const answerElement_tvmaze_3 = document.getElementById('tvmaze-3')
+
+request
+.get('http://api.tvmaze.com/people/10257?embed=castcredits')
+.then(function(response) {
+  var numberOfShows = response.body._embedded.castcredits.length
+  // console.log(numberOfShows)
+  answerElement_tvmaze_3.textContent = numberOfShows;
+})
